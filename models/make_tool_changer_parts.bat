6a8d411f182114296079b48f60ab6c470a3e6f7e
vector76 <vector76@gmail.com>
2020-09-13 16:31:45 -0500

    rename stl files to remove 'tc2_' (not needed to distinguish from other parts)

--- models/make_tool_changer_parts.bat
+++ models/make_tool_changer_parts.bat
@@ -1,14 +1,14 @@
 set OPENSCAD="C:\Program Files\OpenSCAD\openscad.exe"
 rem takes about 24 minues?
 time /T
-%OPENSCAD% tool_changer.scad -D "part=""wire_string_link""" -D double_cleat=1 -o ../STLs/tc2_wire_string_link.stl
-%OPENSCAD% tool_changer.scad -D "part=""drill_guide""" -D double_cleat=1 -o ../STLs/tc2_drill_guide.stl
-%OPENSCAD% tool_changer.scad -D "part=""plate_a""" -D double_cleat=1 -o ../STLs/tc2_plate_a.stl
-%OPENSCAD% tool_changer.scad -D "part=""plate_b""" -D double_cleat=1 -o ../STLs/tc2_plate_b.stl
-%OPENSCAD% tool_changer.scad -D "part=""tool_plate""" -D double_cleat=1 -o ../STLs/tc2_tool_plate.stl
-%OPENSCAD% tool_changer.scad -D "part=""pulley""" -D double_cleat=1 -o ../STLs/tc2_pulley.stl
-%OPENSCAD% tool_changer.scad -D "part=""servo_bracket""" -D double_cleat=1 -o ../STLs/tc2_servo_bracket.stl
-%OPENSCAD% tool_changer.scad -D "part=""back_plate""" -D double_cleat=1 -o ../STLs/tc2_back_plate.stl
+%OPENSCAD% tool_changer.scad -D "part=""wire_string_link""" -D double_cleat=1 -o ../STLs/wire_string_link.stl
+%OPENSCAD% tool_changer.scad -D "part=""drill_guide""" -D double_cleat=1 -o ../STLs/drill_guide.stl
+%OPENSCAD% tool_changer.scad -D "part=""plate_a""" -D double_cleat=1 -o ../STLs/plate_a.stl
+%OPENSCAD% tool_changer.scad -D "part=""plate_b""" -D double_cleat=1 -o ../STLs/plate_b.stl
+%OPENSCAD% tool_changer.scad -D "part=""tool_plate""" -D double_cleat=1 -o ../STLs/tool_plate.stl
+%OPENSCAD% tool_changer.scad -D "part=""pulley""" -D double_cleat=1 -o ../STLs/pulley.stl
+%OPENSCAD% tool_changer.scad -D "part=""servo_bracket""" -D double_cleat=1 -o ../STLs/servo_bracket.stl
+%OPENSCAD% tool_changer.scad -D "part=""back_plate""" -D double_cleat=1 -o ../STLs/back_plate.stl
 %OPENSCAD% core_addon.scad -D short=0 -o ../STLs/core_addon.stl
 %OPENSCAD% core_addon.scad -D short=1 -o ../STLs/core_addon_short.stl
 %OPENSCAD% universal_tool_plate.scad -o ../STLs/universal_tool_plate.stl
